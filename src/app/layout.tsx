efe312447ad161bcb4c9dfc6ef1c6f4609ee4e88
TarunaAgrawal13 <agrawaltaruna13@gmail.com>
2026-05-06 10:26:29 +0530

    navbar done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,16 +9,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{ //itst typeScript saying only react like childrens are acceptable
+}: Readonly<{ //its typeScript saying only react like childrens are acceptable
   children: React.ReactNode;
 }>) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className=""
     >
       <body className="min-h-full flex flex-col">
-      <h2>Nav Item</h2>
+      <div className="relative w-full flex items-center justify-center">
+        <Navbar />
+      </div>
         {children}
      </body>
     </html>
